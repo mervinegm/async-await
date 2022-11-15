f73c7bc7bc9285f7a4a5a15d844ec37e158b6734
Mervin González <gonzalez.mervin@gmail.com>
2022-11-15 15:48:11 -0300

    primer commit

---
+++ models.ts
@@ -0,0 +1,44 @@
+import * as jsonfile from "jsonfile";
+
+class Contact {
+  id?: number = undefined;
+  name: string = "";
+}
+
+class ContactsCollection {
+  data: Contact[] = [];
+  async load() {
+    const promesa = await jsonfile.readFile(__dirname + "/contacts.json");
+    //console.log(promesa);
+    this.data = promesa;
+    return promesa;
+  }
+  getAll() {
+    return this.data;
+  }
+  addOne(contact: Contact) {
+    this.data.push(contact);
+  }
+  async save() {
+    const promesa = await jsonfile.writeFile(
+      __dirname + "/contacts.json",
+      this.data
+    );
+    console.log(promesa);
+    return promesa;
+  }
+  getOneById(id) {
+    const encontrado = this.data.find((contacto) => {
+      if (contacto?.id == id) {
+        return true;
+      }
+    });
+
+    return encontrado;
+  }
+}
+export { ContactsCollection, Contact };
+
+const coll = new ContactsCollection();
+coll.load();
+console.log(coll.data);
